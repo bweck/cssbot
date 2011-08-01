13653b07a9994e463b77af16341c9f2e83961f16
Brian Weck <bweck@weckconsulting.com>
2011-08-01 13:07:09 +1000

    catch io error during execution to a pipe

--- view-queue.py
+++ view-queue.py
@@ -4,7 +4,7 @@
 # Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 #
 
-import time, ConfigParser
+import sys, time, ConfigParser
 import pymongo
 
 # read in config.
@@ -17,20 +17,23 @@ db = mongo[config.get("mongo", "db_name")]
 collection = db[config.get("mongo", "collection_posts")]
 
 # spin.
-for thread in collection.find({'solved':'N'}).sort('next_check', pymongo.ASCENDING):
-   now = time.time()
-   when = thread['next_check']
-   if when < now:
-      when = "+"
-   elif when < (now+60):
-      when = "%ds" % ((when-now))
-   elif when < (now+3600):
-      when = "%dm" % ((when-now)/60)
-   elif when < (now+86400):
-      when = "%.1fh" % ((when-now)/3600)
-   else:
-      when = "%.1fd" % ((when-now)/86400)
+try:
+   for thread in collection.find({'solved':'N'}).sort('next_check', pymongo.ASCENDING):
+      now = time.time()
+      when = thread['next_check']
+      if when < now:
+         when = "+"
+      elif when < (now+60):
+         when = "%ds" % ((when-now))
+      elif when < (now+3600):
+         when = "%dm" % ((when-now)/60)
+      elif when < (now+86400):
+         when = "%.1fh" % ((when-now)/3600)
+      else:
+         when = "%.1fd" % ((when-now)/86400)
 
-   print "%s\t%s\t%s" % (when, thread['id'], (thread['title']).replace('\n', ' '))
+      print "%s\t%s\t%s" % (when, thread['id'], (thread['title']).replace('\n', ' '))
+except IOError, e:
+   sys.exit()
 
 
